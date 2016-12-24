96c828053baf32668e370442e82f4a1ef31cc6a6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-12-24 00:03:13 -0600

    operaciones

---
+++ basico/operaciones.r
@@ -0,0 +1,10 @@
+# Las operaciones se realizan sobre vectores, con elementos correspondientes
+
+v1 <- 1:3
+v2 <- 10:12
+
+v1 + v2 # produce [11, 13, 15]
+
+v3 < 1
+
+v1 + v3 # produce [2, 3, 4] => el vector más pequeño se repite hasta alcanzar la longitud del otro
