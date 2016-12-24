d44983d3dcd00d23b00aedd8e14424d29f56dd5a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-12-23 23:36:04 -0600

    tipos básicos

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 My exercises on R Progrmming. I'm Following the Coursera's course
 (R Programming)[https://www.coursera.org/learn/r-programming].
+
+y 
+
+(Programación estadísitca con R)[https://www.coursera.org/learn/intro-data-science-programacion-estadistica-r]
---
+++ basico/asignacion.r
@@ -0,0 +1,22 @@
+# asignación básica
+mi_numero <- 4 # crea un vector de longitud 1 en la  variable mi_numero
+
+mi_rango <- 1:25 # crea un vector con los valores del 1 al 25 guardados en mi_rango
+
+# Tipos
+
+# Numeric
+5L # Entero
+3 + 5i # Complejo
+5 # Flotante de Doble precisión (por default si no se incluye la 'L' usada en el ejemplo aneterior)
+
+# Logic
+TRUE
+FALSE
+T # Equivalente a TRUE
+F # Equivalente a FALSE
+
+
+# Character
+
+"Hello R" # Cadena de caracteres
