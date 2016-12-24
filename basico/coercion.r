a8736e942ec18413adaaa115ec4e43efa3659e26
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-12-23 23:50:57 -0600

    coerción y vectores

---
+++ basico/coercion.r
@@ -0,0 +1,5 @@
+# Si al declarar un vector, no todos los elementos
+# son del mismo tipo, R intentará hacerlos del mismo, lo que se llama "coerción"
+# pudiendo dar resultados inesperados.
+
+un_vector_mas <- c(1, "1.89", 12) # todo lo convertirá a tipo character
---
+++ basico/vectores.r
@@ -0,0 +1,11 @@
+# Por defecto las variables son vectores, aunque sean de
+# longitud 1
+
+mi_variable <- 12 # vector de 1 componente (miembro) con el valor numérico 12
+
+# creación de vectores de más de un elemento
+
+mi_vector <- c(12, 45, 23, 90, 14) # no se especifica el tipo, pero todos deben ser del mismo | c es por 'combine'
+
+
+mi_vector_explicito <- vector(mode = "logical", length = 10) # modes = "logical", "integer", "numeric"|"double", "complex", "characteer", "raw"
