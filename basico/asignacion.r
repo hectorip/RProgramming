769270a643e4791d55630edf6c3af9a35ec2ca7c
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2016-12-23 23:38:32 -0600

    separando tipos y asignación

--- basico/asignacion.r
+++ basico/asignacion.r
@@ -3,20 +3,6 @@ mi_numero <- 4 # crea un vector de longitud 1 en la  variable mi_numero
 
 mi_rango <- 1:25 # crea un vector con los valores del 1 al 25 guardados en mi_rango
 
-# Tipos
+# también puede ir para el otro lado
 
-# Numeric
-5L # Entero
-3 + 5i # Complejo
-5 # Flotante de Doble precisión (por default si no se incluye la 'L' usada en el ejemplo aneterior)
-
-# Logic
-TRUE
-FALSE
-T # Equivalente a TRUE
-F # Equivalente a FALSE
-
-
-# Character
-
-"Hello R" # Cadena de caracteres
+42 -> la_respuesta
---
+++ basico/tipos.r
@@ -0,0 +1,19 @@
+# Tipos
+
+# Numeric
+5L # Entero
+3 + 5i # Complejo
+5 # Flotante de Doble precisión (por default si no se incluye la 'L' usada en el ejemplo aneterior)
+NaN # Not a number -> Operaciones matemáticas sin sentido
+Inf # Infinito -> Operaciones con números desproporcionadamente grandes o pequeños
+
+# Logic
+TRUE
+FALSE
+T # Equivalente a TRUE
+F # Equivalente a FALSE
+
+
+# Character
+
+"Hello R" # Cadena de caracteres
